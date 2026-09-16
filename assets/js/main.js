3b476c9365102af5166e6bc99274afc931fd0289
ViniciusSito <itoviniciusshigueyuki@gmail.com>
2026-09-16 16:11:26 -0300

    manipulacao de dados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-const offset = 0
-const limit = 10
-
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-function convertPokemonToHtml(pokemon) {
+function convertPokemonToLi(pokemon) {
     return `<li class="pokemon">
                 <span class="number">#001</span>
                 <span class="name">${pokemon.name}</span>
@@ -19,17 +14,9 @@ function convertPokemonToHtml(pokemon) {
     `
 }
 
-const pokemonList = document.getElementById('pokemonList')
-//o fetch nos retorna uma Promise, que é um objeto que representa a eventual 
-// conclusão (ou falha) de uma operação assíncrona e seu valor resultante.
+const pokemonList = document.getElementById('pokemonList');
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i]
-            pokemonList.innerHTML += convertPokemonToHtml(pokemon)
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML += newHtml
+})
